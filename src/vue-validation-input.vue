32a7bb69d32e87209893e0dafa1d15f61b84330f
riyuuryu <riyuuryu@gmail.com>
2021-04-15 19:50:09 +0800

    Added inputClass & label

--- src/vue-validation-input.vue
+++ src/vue-validation-input.vue
@@ -3,23 +3,31 @@
 </style>
 
 <template>
-  <input 
-  v-model="value"
-  :class="inputClass" 
-  :placeholder="placeholder" 
-  >
+  <div class="">
+    <label :for="`${inputClass}-label`">{{label}}</label>
+    <input 
+    v-model="value"
+    :class="inputClass" 
+    :placeholder="placeholder" 
+    >
+  </div>
 </template>
 
 <script>
 export default {
   name: 'VueValidationInput',
   props: {
+    inputClass: { type: String, required: false, default: 'vue-validation-input'},
+    label: {type: String, required: false, default: '' },
     minLength: { type: Number, required: false, default: 0 },
     maxLength: { type: Number, required: false, default: 0 }, 
     placeholder: { type: String, required: false, default: '' }
   },
   data() {
     return {
+      error: {
+        message: ''
+      },
       valid: false,
       value: '',
     };
@@ -27,10 +35,12 @@ export default {
   computed: {},
   watch: {
     value: function(newValue, oldValue){
-      if(newValue.length >= this.minLength && newValue.length <= this.maxLength) this.valid = true;
-      else this.valid = false;
+      /*if(newValue.length >= this.minLength && newValue.length <= this.maxLength) this.valid = true;
+      else this.valid = false;*/
     }
   },
-  methods: {},
+  methods: {
+    
+  },
 };
 </script>
