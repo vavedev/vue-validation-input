9c56734d0d083123efdec157b9d1b6538a9cfd27
riyuuryu <riyuuryu@gmail.com>
2021-04-17 21:51:41 +0800

    Import Font Awesome

--- dev/serve.js
+++ dev/serve.js
@@ -1,6 +1,11 @@
 import Vue from 'vue';
 import Dev from './serve.vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCheckCircle, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
+library.add([faCheckCircle, faTimesCircle]);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 Vue.config.productionTip = false;
 
 new Vue({
